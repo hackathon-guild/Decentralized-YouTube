7a9c12cc9d9a0eb23f18eef9343dfc11eb4a3f73
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-03-25 00:27:15 +0100

    minor refactor

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Background = ({ children }) => {
   return (
-    <body className="bg-white dark:bg-backgroundBlack h-screen  transition-all">
+    <body className="bg-grey dark:bg-backgroundBlack h-screen  transition-all">
       {children}
     </body>
   );
